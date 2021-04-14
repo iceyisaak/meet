9d18d541e876a7f32b996a0282bea37c3b2ab53e
iceyisaak <me@iceyisaak.com>
2021-04-14 16:30:15 +0200

    Implementing unit tests

---
+++ src/mock-data.js
@@ -0,0 +1,12 @@
+const mockData = [
+  {
+    "kind": "calendar#events",
+    "etag": "\"p3308h454qfgum0g\""
+  },
+  {
+    "kind": "calendar#event",
+    "etag": "\"3181161784712000\""
+  }
+];
+
+export { mockData };
