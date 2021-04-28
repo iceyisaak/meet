16012529a9848bc91aeffe6e2c48af983ccadc93
iceyisaak <me@iceyisaak.com>
2021-04-28 22:07:36 +0200

    Debugged puppeteer Chromium problem

--- src/__test__/EndToEnd.test.js
+++ src/__test__/EndToEnd.test.js
@@ -3,23 +3,24 @@ import puppeteer from 'puppeteer';
 describe('show/hide an event details',
   () => {
 
-    test('An event element is collapsed by default',
-      async () => {
+    let browser;
+    let page;
 
-        const browser = await puppeteer.launch({
-          executablePath: '/usr/bin/chromium-browser'
-        });
-        // const browser = await puppeteer.launch();
-        const page = await browser.newPage();
+    beforeAll(async () => {
+      jest.setTimeout(30000);
+      browser = await puppeteer.launch();
+      page = await browser.newPage();
+      await page.goto('http://localhost:3000/');
+      await page.waitForSelector('.event');
+    });
 
-        await page.goto('http://localhost:3000/');
-        await page.waitForSelector('.event');
+    afterAll(() => {
+      browser.close();
+    });
 
-        const eventDetails = await page.$('.event .event__description--hide');
-        expect(eventDetails).toBeNull();
-        browser.close();
-
-      }
-    );
+    test('An event element is collapsed by default', async () => {
+      const eventDetails = await page.$('.event .event__description--show');
+      expect(eventDetails).toBeNull();
+    });
 
   });
